294d095fc5d9036818c4bbc6dc6d628dfc8a9a1a
TheNameisNavin <navinkumarspm@outlook.com>
2025-05-06 18:32:12 +0530

    Update drbird.cs

--- drbird.cs
+++ drbird.cs
@@ -1,2 +1,24 @@
+using UnityEngine;
 
+public class bird1 : MonoBehaviour
+{
+    public Rigidbody2D myRigidbody;
+    public float flapStrength;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+      if (Input.GetKeyDown(KeyCode.Space) == true)     
+      {
+        myRigidbody.linearVelocity = Vector2.up * flapStrength;
+
+      }
+    }
+}
 
